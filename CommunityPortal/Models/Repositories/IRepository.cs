ebd61d91ad38facf67d97614f67d37b09914e9c8
Rashin Aeini <aeini.rashin@gmail.com>
2021-12-31 13:33:40 +0330

    Create Generic Repository and Service

---
+++ CommunityPortal/Models/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CommunityPortal.Models.Repositories
+{
+    public interface IRepository<T>
+    {
+        List<T> Read();
+        T Read(int id);
+        T Create(T entry);
+        bool Update(T entry);
+        bool Delete(T entry);
+    }
+}
