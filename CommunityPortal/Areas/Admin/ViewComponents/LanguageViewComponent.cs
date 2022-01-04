59755d5e6c33b8a55acdbecef42bc987bc70d2ad
Rashin Aeini <aeini.rashin@gmail.com>
2022-01-04 15:38:18 +0330

    Sample Language ViewComponent

---
+++ CommunityPortal/Areas/Admin/ViewComponents/LanguageViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunityPortal.Areas.Admin.ViewComponents
+{
+    public class LanguageViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(new List<string>(){ "Persian", "English" });
+        }
+    }
+}
