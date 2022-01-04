92a6c0f7136ca598d0579d6ad98b625bb1b6f55b
Rashin Aeini <aeini.rashin@gmail.com>
2022-01-04 15:39:57 +0330

    Save All Changes

--- CommunityPortal/Controllers/HomeController.cs
+++ CommunityPortal/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using CommunityPortal.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CommunityPortal.Controllers
 {
--- CommunityPortal/Models/ErrorViewModel.cs
+++ CommunityPortal/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace CommunityPortal.Models
 {
     public class ErrorViewModel
