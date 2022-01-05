83badcd2946637cd2d90b0900e864cf8e11df5af
Rashin Aeini <aeini.rashin@gmail.com>
2022-01-05 15:18:06 +0330

    Fetching data from Service in List action

--- CommunityPortal/Areas/Admin/Controllers/CategoryController.cs
+++ CommunityPortal/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CommunityPortal.Areas.Admin.Models.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CommunityPortal.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
+        private CategoryService Service { get; }
+
+        public CategoryController(CategoryService service)
+        {
+            Service = service;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult List()
+        {
+            return Ok(Service.GetAll());
         }
     }
 }
