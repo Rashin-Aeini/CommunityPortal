df207eb438fcb2c7358aeae7e328fb36397c40d8
Rashin Aeini <aeini.rashin@gmail.com>
2022-01-05 15:20:41 +0330

    Create React components for category

---
+++ CommunityPortal/wwwroot/js/category/App.jsx
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import Axios from 'axios';
+
+const App = () => {
+
+    React.useEffect(() => {
+        Axios.get('Category/List').then(function (response) {
+            console.log(response.data);
+        });
+    }, []);
+
+    return (<h1>this is a sample</h1>);
+}
+
+export default App;
