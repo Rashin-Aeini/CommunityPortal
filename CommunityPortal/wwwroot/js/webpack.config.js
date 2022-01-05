e9269fd135a5341750f3388964d59620697202d6
Rashin Aeini <aeini.rashin@gmail.com>
2022-01-05 15:20:05 +0330

    Install and config Node.JS in ASP.Net

---
+++ CommunityPortal/wwwroot/js/webpack.config.js
@@ -0,0 +1,28 @@
+﻿const path = require("path");
+
+module.exports = function (env, argv) {
+    const isProduction = argv.mode === "production";
+    const isDevelopment = !isProduction;
+
+    return {
+        devtool: isDevelopment && "cheap-module-source-map",
+        entry: './category/main.jsx',
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader']
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.js', '.jsx']
+        },
+        output: {
+            path: path.resolve(__dirname, "./"),
+            filename: 'category.js',
+            publicPath: './'
+        }
+    };
+};
