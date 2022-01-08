6fc11996a5e05d713ef32aff78dde03814edeb77
Rashin Aeini <aeini.rashin@gmail.com>
2022-01-08 21:42:24 +0330

    Complete Category Controller

--- CommunityPortal/Areas/Admin/Controllers/CategoryController.cs
+++ CommunityPortal/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,10 @@
-﻿using CommunityPortal.Areas.Admin.Models.Services;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommunityPortal.Areas.Admin.Models.Services;
+using CommunityPortal.Areas.Admin.Models.ViewModels.Category;
+using CommunityPortal.Models.Domains;
+using CommunityPortal.Models.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommunityPortal.Areas.Admin.Controllers
@@ -13,14 +19,106 @@ namespace CommunityPortal.Areas.Admin.Controllers
             Service = service;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(FilterCategoryViewModel entry)
+        {
+            const int size = 20;
+
+            List<Category> categories = Service.GetAll()
+                .Where(item => item.Title.Contains(entry.Search))
+                .ToList();
+
+            entry.Pages = (int)Math.Ceiling(categories.Count / (double)size);
+
+            int offset = entry.Page > 1 ? (entry.Page - 1) * size : 0;
+
+            entry.Result.AddRange(categories
+                .Skip(offset)
+                .Take(size)
+                .Select(item => new IndexCategoryViewModel()
+                {
+                    Id = item.Id,
+                    Title = item.Title
+                })
+                .ToList());
+
+            return View(entry);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
 
-        public IActionResult List()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CreateCategoryViewModel entry)
         {
-            return Ok(Service.GetAll());
+            if (!ModelState.IsValid)
+            {
+                return View(entry);
+            }
+
+            Service.Add(entry);
+
+            TempData.Message("Rashin", "Your category saved successfully");
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Category entry = Service.GetById(id);
+
+            if (entry == null)
+            {
+                TempData.Message("Rashin", "The category doesn't exist");
+                return RedirectToAction(nameof(Index));
+            }
+
+            CreateCategoryViewModel model = new CreateCategoryViewModel()
+            {
+                Title = entry.Title
+            };
+            ViewBag.Id = entry.Id;
+
+            return View(model);
+        }
+        
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult Edit(int id, CreateCategoryViewModel entry)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Id = id;
+                return View(entry);
+            }
+
+            if (Service.Edit(id, entry))
+            {
+                TempData.Message("Rashin", "The category updated");
+            }
+            else
+            {
+                TempData.Message("Rashin", "Have some problem in updating, please try later");
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (Service.Remove(id))
+            {
+                TempData.Message("Rashin", "The category delete");
+            }
+            else
+            {
+                TempData.Message("Rashin", "Have some problem in updating, please try later");
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
