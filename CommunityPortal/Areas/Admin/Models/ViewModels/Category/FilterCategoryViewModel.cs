4f62c44c64e3adecc50427a197d00f41738e7092
Rashin Aeini <aeini.rashin@gmail.com>
2022-01-08 21:43:20 +0330

    Change some category related file

--- CommunityPortal/Areas/Admin/Models/ViewModels/Category/FilterCategoryViewModel.cs
+++ CommunityPortal/Areas/Admin/Models/ViewModels/Category/FilterCategoryViewModel.cs
@@ -7,13 +7,22 @@ namespace CommunityPortal.Areas.Admin.Models.ViewModels.Category
         public FilterCategoryViewModel()
         {
             Result = new List<IndexCategoryViewModel>();
+            Search = ""; //string.Empty;
         }
 
+        #region Question
+        
         public string Search { get; set; }
 
         public int Page { get; set; }
         public int Pages { get; set; }
 
+        #endregion
+
+        #region Answer
+        
         public List<IndexCategoryViewModel> Result { get; }
+
+        #endregion
     }
 }
